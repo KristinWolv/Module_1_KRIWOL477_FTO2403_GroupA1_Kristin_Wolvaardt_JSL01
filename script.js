026446abadde8506aa096308a458a1282977084a
KristinWolv <162977890+KristinWolv@users.noreply.github.com>
2024-05-09 00:47:20 +0200

    Module_1_KRIWOL477_FTO2403_GroupA1_Kristin_Wolvaardt_JSL01

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    //These variables indicates whether the prefix, birthyear and name are valid
+    //Initialized as 'false'
+    let petPrefixValid = false;
+    let petBirthYearValid = false;
+    let petNameValid = false;
+    //Stores the result
+    let result = ''; 
+   
+    //Converts input string to lowercase
+    input = petInput.toLowerCase();
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    //Checks the firts 4 characters and checks if "pet_" is included
+    let prefix = input.slice(0, 4);
+    petPrefixValid = prefix.includes("pet_");
 
-            document.getElementById('result').innerText = result;
+    //Extracts characters representing the birth year of pet, converts to number and checks if it falls between 2000 to 2024
+    let petBirthYear = input.slice(4, 8);
+    petBirthYear = +petBirthYear;
+
+    if (petBirthYear >= 2000 && petBirthYear <= 2024) {
+        petBirthYearValid = true;
+    }
+
+    //Extracts character representing the pet name and chects if the length is greater than 0
+    let petName = input.slice(8);
+    if (petName.length > 0); {
+        petNameValid = true;
+    }
+
+    //checks if the input field is empty. If yes it sets 'result' to inticate error
+    //Checks if all validation are 'true' , if yes, sets 'result' to indicate valid syntax or invalid syntax
+    if (petInput === " || petInput == null") {
+        result = "Error: Input Field Empty";
+    }
+    else if (petPrefixValid && petBirthValid && petNameValid) {
+        result = "Valid Syntax";
+    }
+    else {
+        result = "Invalid Syntax";
+    }
+
+
+    document.getElementById('result').innerText = result;
 }
 
 
